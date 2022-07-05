f62d1a2cdbd1c29a54d3ccc64c1fed842e4d2b32
BSH <bsh93367@gmail.com>
2022-07-05 13:30:16 +0900

    Update 1844_게임 맵 최단거리.js
    효율성 테스트 통과 못했는데, 살짝 줄이니 통과함

--- Programmers/1844_게임 맵 최단거리.js
+++ Programmers/1844_게임 맵 최단거리.js
@@ -46,55 +46,57 @@ solution([
   [1, 1, 1, 0, 1],
   [0, 0, 0, 0, 1],
 ]);
-function check(matrix, visited) {
-  let newX, newY, xy;
-  let xys = [[0, 0]];
+// solution([
+//   [1, 0, 1, 1, 1, 1, 1],
+//   [1, 0, 1, 0, 1, 0, 1],
+//   [1, 0, 1, 1, 1, 0, 1],
+//   [1, 1, 1, 0, 1, 0, 1],
+//   [0, 0, 1, 0, 0, 0, 1],
+//   [0, 0, 1, 0, 1, 0, 1],
+//   [0, 0, 1, 1, 1, 0, 1],
+// ]);
+// solution([
+//   [1, 1, 1, 1, 1, 0, 0],
+//   [1, 0, 1, 0, 1, 1, 1],
+//   [1, 1, 1, 1, 1, 0, 1],
+// ]);
+function check(matrix) {
+  let newX, newY;
+  let xys = [[0, 0, 1]];
+  matrix[0][0] = 0;
   rowD = [0, 0, 1, -1];
   colD = [1, -1, 0, 0];
-  while (true) {
-    if (xys.length === 0) {
-      return -1;
-    }
-    xy = xys.shift();
+  while (xys.length) {
+    let [y, x, count] = xys.shift();
     for (i = 0; i < 4; i++) {
-      newY = xy[0] + colD[i];
-      newX = xy[1] + rowD[i];
+      newY = y + colD[i];
+      newX = x + rowD[i];
       if (
-        (newX, newY) !== (0, 0) &&
         newX >= 0 &&
         newY >= 0 &&
         newY <= matrix.length - 1 &&
         newX <= matrix[0].length - 1 &&
-        visited[newY][newX] === false &&
         matrix[newY][newX] === 1
       ) {
-        visited[newY][newX] = true;
-        matrix[newY][newX] = matrix[xy[0]][xy[1]] + 1;
-        xys.push([newY, newX]);
-        if (newX === matrix.length - 1 && newY === matrix[0].length - 1) {
-          return matrix[newY][newX];
+        if (newY === matrix.length - 1 && newX === matrix[0].length - 1) {
+          return count + 1;
         }
+        matrix[newY][newX] = 0;
+        xys.push([newY, newX, count + 1]);
       }
     }
   }
+  return -1;
 }
 function solution(arr) {
   let matrix = [];
-  let visited = [];
-  let a, b;
+  let a;
   for (i = 0; i < arr.length; i++) {
     a = [];
-    b = [];
     for (j = 0; j < arr[0].length; j++) {
       a.push(arr[i][j]);
-      b.push(false);
     }
     matrix.push(a);
-    visited.push(b);
   }
-  console.log(matrix);
-  console.log(visited);
-  let ansewer = check(matrix, visited);
-  console.log(ansewer);
-  return ansewer;
+  return check(matrix);
 }
