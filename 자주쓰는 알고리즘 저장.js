1c39314af78c4c0181d54df4573520be04470c6c
SeungHak5161 <bsh93367@gmail.com>
2022-07-09 23:30:27 +0900

    Update 자주쓰는 알고리즘 저장.js

--- 자주쓰는 알고리즘 저장.js
+++ 자주쓰는 알고리즘 저장.js
@@ -29,3 +29,45 @@ function binary(n) {
   }
   return arr;
 }
+
+function isalpha(c) {
+  return (c >= "a" && c <= "z") || (c >= "A" && c <= "Z");
+}
+
+function isdigit(c) {
+  return c >= "0" && c <= "9";
+}
+
+function isalnum(c) {
+  return isalpha(c) || isdigit(c);
+}
+
+/* [특수 문자 제거 이벤트 수행 부분] */
+function removeSpecialData(originalData) {
+  console.log(originalData);
+
+  // [1] 정규식 형태 선언 (특수문자, 괄호, 점, 공백 모두 제거 실시)
+  var reg = /[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/ ]/gim;
+
+  // [2] 정규식 형태 선언 (특수문자, 괄호, 점 모두 제거 실시 - 공백은 제거 안함)
+  //var reg = /[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gim;
+
+  // [3] 정규식 형태 선언 (특수문자, 괄호, 공백 모두 제거 실시 - 점은 제거 안함)
+  //var reg = /[`~!@#$%^&*()_|+\-=?;:'"<>\{\}\[\]\\\/ ]/gim;
+  var resultData = originalData.replace(reg, "");
+  console.log(resultData);
+}
+
+/* [숫자 제거 이벤트 수행 부분] */
+function removeNumberData(originalData) {
+  console.log(originalData);
+  var reg = /[0-9 ]/gim;
+  var resultData = originalData.replace(reg, "");
+  console.log(resultData);
+}
+
+/* [영어 제거 이벤트 수행 부분] */
+function removeEngData(originalData) {
+  var reg = /[a-zA-Z ]/gim;
+  var resultData = originalData.replace(reg, "");
+  console.log(resultData);
