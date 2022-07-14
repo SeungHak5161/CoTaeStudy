6b519d38527fc29f1a33077fc86b74913fcbcc59
BSH <bsh93367@gmail.com>
2022-07-14 16:07:47 +0900

    Update 12978_배달.js

--- Programmers/12978_배달.js
+++ Programmers/12978_배달.js
@@ -1,23 +1,31 @@
+// 시간초과 4개 core dumped 3개나와서 78.1
 let N = 5;
 let road = [
-  [1, 2, 1],
-  [2, 3, 3],
-  [5, 2, 2],
-  [1, 4, 2],
-  [5, 3, 1],
-  [5, 4, 2],
+  [1, 2, 3],
+  [1, 3, 1],
+  [1, 5, 8],
+  [2, 4, 5],
+  [2, 3, 2],
+  [3, 5, 2],
+  [5, 4, 1],
 ];
-let K = 3;
+let K = 7;
 solution(N, road, K);
 
-function check(Head, road) {
+function makeTree(Head, road, N, K, numList) {
+  let i = 0;
   road.forEach((e) => {
-    console.log(Head.num);
-    if (e[0] === Head.num) {
+    // console.log(e, Head.num);
+    if (e[0] === Head.num && Head.distance + e[2] <= K) {
       Head.insertChild({ num: e[1], distance: e[2] });
-      check(Head.children[0], road);
+      if (numList.indexOf(e[1]) !== -1) {
+        numList.splice(numList.indexOf(e[1]), 1);
+      }
+      makeTree(Head.children[i], road, N, K, numList);
+      i++;
     }
   });
+  i = 0;
 }
 
 function solution(N, road, K) {
@@ -36,7 +44,13 @@ function solution(N, road, K) {
   }
   const Head = new makeNode({ num: 1, distance: 0 });
 
+  let numList = [];
   let roadRev = [];
+  for (let i = 1; i <= N; i++) {
+    numList.push(i);
+  }
+  numList.shift();
+
   // 순차적으로 트리에 삽입하기 위해 정렬
   road.forEach((e) => {
     if (e[0] > e[1]) {
@@ -45,9 +59,19 @@ function solution(N, road, K) {
       e[1] = temp;
     }
   });
-  console.log(road);
-  check(Head, road);
+  for (let i = 0; i < road.length; i++) {
+    let temp = [];
+    temp[0] = road[i][1];
+    temp[1] = road[i][0];
+    temp[2] = road[i][2];
+    roadRev.push(temp);
+  }
+  let roads = [...road, ...roadRev];
+  makeTree(Head, roads, N, K, numList);
+  console.log(roads);
   console.log(Head);
+  console.log(numList);
+  console.log(N - numList.length);
 
-  // return answer;
+  return N - numList.length;
 }
